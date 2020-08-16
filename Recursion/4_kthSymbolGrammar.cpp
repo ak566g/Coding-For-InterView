7aa45ce887e0b37ca3b53465ddc8f009347b57e2
Ankita Gupta <66826303+ak566g@users.noreply.github.com>
2020-08-16 17:26:08 +0530

    kth-symbol-corrected

--- Recursion/4_kthSymbolGrammar.cpp
+++ Recursion/4_kthSymbolGrammar.cpp
@@ -1,10 +1,10 @@
 // Problem Statement
 // k       1 2 3 4 5 6 7 8 9 10..........
-// n= 0    0
-// n= 1    0 1
-// n= 2    0 1 1 0
-// n= 3    0 1 1 0 1 0 0 1
-// n= 4    0 1 1 0 1 0 0 1 1 0 0 1 0 1 1 0
+// n= 1    0
+// n= 2    0 1
+// n= 3    0 1 1 0
+// n= 4    0 1 1 0 1 0 0 1
+// n= 5    0 1 1 0 1 0 0 1 1 0 0 1 0 1 1 0
 // For each next line 0 will converted into 0 1 and 1 will converted into 1 0
 // n and k will be given , return value at n,k.
 
@@ -17,14 +17,14 @@ int kth_symbol(int n, int k){
         return 0;
     }
 
-    int mid= (int)pow(2,n-1)/2;
+    int mid= pow(2,n-1)/2;
 
-    if(mid>k){
+    if(k<=mid){
+        return kth_symbol(n-1,k);
+    }else{
         return !kth_symbol(n-1,k-mid);
     }
-    else{
-        return kth_symbol(n-1,k);
-    }
+
 }
 
 int main()
