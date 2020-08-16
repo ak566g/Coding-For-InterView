b3b9a5e5a0b78b1ce0a80d62ab8c7c7e46436fcd
Ankita Gupta <66826303+ak566g@users.noreply.github.com>
2020-08-16 17:15:46 +0530

    kth-symbol

---
+++ Recursion/4_kthSymbolGrammar.cpp
@@ -0,0 +1,36 @@
+// Problem Statement
+// k       1 2 3 4 5 6 7 8 9 10..........
+// n= 0    0
+// n= 1    0 1
+// n= 2    0 1 1 0
+// n= 3    0 1 1 0 1 0 0 1
+// n= 4    0 1 1 0 1 0 0 1 1 0 0 1 0 1 1 0
+// For each next line 0 will converted into 0 1 and 1 will converted into 1 0
+// n and k will be given , return value at n,k.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int kth_symbol(int n, int k){
+    if(n==1&&k==1){
+        return 0;
+    }
+
+    int mid= (int)pow(2,n-1)/2;
+
+    if(mid>k){
+        return !kth_symbol(n-1,k-mid);
+    }
+    else{
+        return kth_symbol(n-1,k);
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    cout<<kth_symbol(n,k);
+}
+
---
+++ Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+int mid= (int)pow(2,n)/2;
+
+    // if(mid>k){
+    //     return !kth_symbool(n-1,k-mid);
+    // }
+    // else{
+    //     return kth_symbool(n-1,k);
+    // }
