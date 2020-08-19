1efbd632a5127dcc976ef9bbb7116eb19aad6c7e
Ankita Gupta <66826303+ak566g@users.noreply.github.com>
2020-08-19 14:39:52 +0530

    first-occurence

--- Searching/2_FindFirstOccurence.cpp
+++ Searching/2_FindFirstOccurence.cpp
@@ -5,8 +5,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int firstOccurence(int *a, int n, int element)
+{
+    int start= 0, end = n-1;
+    int ans=-1;
+
+    while(start<=end)
+    {
+        int mid = start + (end - start)/2;
+
+        if(a[mid]==element)
+        {
+            ans=mid;
+            end = mid-1;
+        }
+        else if(a[mid]<element)
+        {
+            start = mid+1;
+        }
+        else
+        {
+            end = mid-1;
+        }
+        
+    }
+
+    return ans;
+}
+
+
 int main(){
 
-    
+    int n;
+    cin>>n;
+
+    int *a = new int[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int element;
+    cin>>element;
+
+    cout<<firstOccurence(a,n,element);
 
 }
