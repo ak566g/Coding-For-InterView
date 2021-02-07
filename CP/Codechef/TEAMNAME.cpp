302f61f7bfae78f1045b616ff377965295a56b85
ak566g <anki01.g@gmail.com>
2021-02-07 16:44:12 +0530

    team name

--- CP/Codechef/TEAMNAME.cpp
+++ CP/Codechef/TEAMNAME.cpp
@@ -11,6 +11,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int common(vector <string> v1, vector <string> v2){
+    int intersection = 0;
+    int n1 = v1.size(), n2 = v2.size();
+
+    int i =0, j = 0;
+
+    while(i<n1 && j<n2){
+        if(v1[i]==v2[j]){
+            intersection++;
+            i++;
+            j++;
+        }
+        else if(v1[i]<v2[j]){
+            i++;
+        }
+        else{
+            j++;
+        }
+    }
+
+    return intersection;
+}
+
 int main(){
     int t;
     cin>>t;
@@ -20,30 +43,38 @@ int main(){
         cin>>n;
 
         vector<string>v(n);
-        unordered_map<string, int>um;
         for(int i=0; i<n; i++){
             cin>>v[i];
-            um[v[i]]=1;
+        }
+        
+        unordered_map<char, vector<string> >um;
+        for(int i = 0; i<26; i++){
+            vector<string>temp;
+            um[i+'a'] = temp;
         }
 
-
+        for(int i = 0; i<n ;i++){
+            char ch = v[i][0];
+            string s = v[i].substr(1);
+            um[ch].push_back(s);
+        }
+        for(int i = 0 ;i < 26; i++){
+            sort(um[i+'a'].begin(), um[i+'a'].end());
+        }
         int ans = 0;
+        for(int i = 0; i<26; i++){
+            for(int j = 0; j <26 ; j++){
+                char index1 = i+'a';
+                char index2 = j+'a';
+                int intersection = common(um[index1], um[index2]);
 
-        for(int i = 0; i<n ; i++){
-            for(int j = i+1; j<n; j++){
-                if(v[i][0]!=v[j][0]){
-                    string a = v[i];
-                    string b = v[j];
+                int n1 = um[index1].size()-intersection;
+                int n2 = um[index2].size()-intersection;
 
-                    swap(a[0], b[0]);
-
-                    if(um[a]==0 && um[b]==0){
-                        ans++;
-                    }
-                }
+                ans += (n1*n2);
             }
         }
 
-        cout<<ans*2<<"\n";
+        cout<<ans<<"\n";
     }
 }
